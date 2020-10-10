4107326772eec2b1d4ed232881c3960d11cbedc0
Maritza Stabile <maritza723@yahoo.com>
2020-10-10 14:04:04 -0700

    algorithm practice

---
+++ easy/1507ReformatDate.js
@@ -0,0 +1,75 @@
+/**1507. Reformat Date
+ * Given a date string in the form Day Month Year, where:
+ * 
+ * Day is in the set {"1st", "2nd", "3rd", "4th", ..., "30th", "31st"}.
+ * Month is in the set {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}.
+ * Year is in the range [1900, 2100].
+ * Convert the date string to the format YYYY-MM-DD, where:
+ * 
+ * YYYY denotes the 4 digit year.
+ * MM denotes the 2 digit month.
+ * DD denotes the 2 digit day.
+ *  
+ * Example 1:
+ * Input: date = "20th Oct 2052"
+ * Output: "2052-10-20"
+ * 
+ * Example 2:
+ * Input: date = "6th Jun 1933"
+ * Output: "1933-06-06"
+ * 
+ * Example 3:
+ * Input: date = "26th May 1960"
+ * Output: "1960-05-26" *//**
+ * @param {string} date
+ * @return {string}
+ */
+var reformatDate = function(date) {
+    let len = date.length - 1;
+    let dict = {
+        "Jan": "01", 
+        "Feb": "02", 
+        "Mar": "03",
+        "Apr": "04",
+        "May": "05",
+        "Jun": "06",
+        "Jul": "07",
+        "Aug": "08",
+        "Sep": "09",
+        "Oct": "10",
+        "Nov": "11",
+        "Dec": "12"
+    }
+    let i = 3;
+    let day = "";
+    if(date[i] != " "){
+        i = 5;
+        day = date.substring(0, 2)
+    } else { 
+        i = 4; 
+        day = "0" + date.substring(0, 1);
+    }
+    let monInDict = "";
+    // console.log(i);
+    while (date[i] != " "){
+        
+        monInDict = monInDict + date[i];
+        i++;
+    }
+    // console.log(monInDict)
+
+    
+    let month = dict[monInDict];
+    let year = date.substring(len - 3);
+    return year + "-" + month + "-" + day;
+     
+    
+};
+
+// Accepted!! 10.10.2020
+// Runtime: 72 ms, faster than 85.59% of JavaScript online submissions for Reformat Date.
+// Memory Usage: 38.7 MB, less than 24.77% of JavaScript online submissions for Reformat Date.
+
+console.log(reformatDate("20th Oct 2052") + " == 2052-10-20");
+console.log(reformatDate("6th Jun 1933") + " == 1933-06-06");
+console.log(reformatDate("26th May 1960") + " == 1960-05-26"); 
